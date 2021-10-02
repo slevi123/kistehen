d913a2f85fe4faf42d229e5ac9d6a51eba1adfb5
Morosanu Norbert <kesetjoaca@gmail.com>
2021-10-02 17:41:10 +0300

    ALAP

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+        <link rel="manifest" href="site.webmanifest">
+        <link rel="apple-touch-icon" href="icon.png">
+    
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/index.css" >
+    
+        <meta name="theme-color" content="#fafafa">
+    </head>
+    <body id="index-page">
+        <div class="description">
+            <div class="desc-text">
+                <h1>Looking for an online job as a freelancer ?</h1>
+                <h1>You've started a company but need more people to work with ?</h1>
+                <h2>We're glad you came in here where you could find many job listings from companies around the world, many talented freelancers looking to work for your company. </h2>
+            </div>
+        </div>
+        <div class="section-wrapper">
+            <div class="jobgiver">
+                <h2>For companies...</h2>
+                <a href="">Take me there!</a>
+            </div>
+            <div class="jobsearcher">
+                <h2>For freelancers</h2>
+                <a href="">Take me there!</a>
+            </div>
+        </div>
+    </body>
+</html>
